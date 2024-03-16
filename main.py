086979b7b284001d83330b0f19f017f3ee0152eb
Marcos <marcosdevelopment.business@gmail.com>
2024-03-16 19:39:01 -0300

    feat: allow predict by passing the total of experience as parameter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from utils import plot
+from sys import argv
 
 dataset = pd.read_csv('data.csv')
 
@@ -20,3 +21,13 @@ plot('Real VS. Predicted - Train set', X_train, y_train, y_train_predictions)
 y_test_predictions = regressor.predict(X_test)
 
 plot('Real VS. Predicted - Test set', X_test, y_test, y_test_predictions)
+
+if len(argv) == 2:
+    years_of_experience = int(argv[1])
+
+    prediction = regressor.predict([[years_of_experience]])
+
+    print(f'[+] An employee with {years_of_experience} years of experience should earn about {prediction[0]:.2f}')
+else:
+    print('You can specify the years of experience to get a prediction of salary by passing it as a parameter')
+    print('Example: python3 ./main.py 10')
